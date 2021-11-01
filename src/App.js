b6292db662c27204279852974ee2d6a65688743a
Manishav233 <manishav233@gmail.com>
2021-11-01 17:07:51 +0530

    user_list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// export default function App() {
 
-export default App;
+//   return (
+//     <div className="App">
+// <Msg name="mani"/>
+// <Msg name="anu"/>
+// <Msg name="megha"/>
+//     </div>
+//   );
+// }
+
+// function Msg(props){
+//   return(
+//     <div>
+//       <h2>Hello World{props.name} 😊</h2>
+//     </div>
+//   );
+// }
+
+// export default function App() {
+//   const names=["mani","anu","megha"];
+//   return (
+//     <div className="App">
+// {names.map(nm=><Msg name={nm}/>)}
+//     </div>
+//   );
+// }
+
+// function Msg({name}){
+//   return(
+//     <div>
+//       <h2>Hello World{name} 😊</h2>
+//     </div>
+//   );
+// }
+
+
